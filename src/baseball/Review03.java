f2aa9eb2580e6351162552ce93d9386ef903e908
ryoutakimura <yellow11053@gmail.com>
2023-11-14 23:37:47 +0900

    コメント追記

--- src/baseball/Review03.java
+++ src/baseball/Review03.java
@@ -3,7 +3,7 @@ package baseball;
 public class Review03 {
 
     public static void main(String[] args) {
-        //球団のデータを生成
+        //球団のインスタンスを生成
         BaseBallTeam swallows = new BaseBallTeam("東京ヤクルトスワローズ",80,59,4);
         BaseBallTeam baystars = new BaseBallTeam("横浜DeNAベイスターズ",73,68,2);
         BaseBallTeam tigers = new BaseBallTeam("阪神タイガース",68,71,4);
@@ -11,6 +11,7 @@ public class Review03 {
         BaseBallTeam carp = new BaseBallTeam("広島東洋カープ",66,74,3);
         BaseBallTeam dragons = new BaseBallTeam("中日ドラゴンズ",66,75,2);
 
+        //勝敗情報の表示
         swallows.report();
         baystars.report();
         tigers.report();
