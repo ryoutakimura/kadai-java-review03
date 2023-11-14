550a6c51b30bda926e65d48b8b601946d7a34d06
ryoutakimura <yellow11053@gmail.com>
2023-11-14 23:30:25 +0900

    勝率を計算するメソッドの実装＆メッセージの表示

--- src/baseball/BaseBallTeam.java
+++ src/baseball/BaseBallTeam.java
@@ -11,7 +11,7 @@ public class BaseBallTeam {
     public BaseBallTeam() {
 
     }
-
+    //引数3つのコンストラクタ
     public BaseBallTeam(String name, int win, int lose,int draw) {
         this.name = name;
         this.win = win;
@@ -19,9 +19,16 @@ public class BaseBallTeam {
         this.draw = draw;
     }
 
+    //勝率を計算してdouble型で返すメソッド
+    public double getRate() {
+        //勝率＝勝ち数/(勝ち数+負け数)
+        double rate = (double)this.win / (this.win +this.lose);
+        return rate;
+    }
+
     //試合結果を表示するメソッド
     public void report() {
-        System.out.println(this.name + "の2022年の成績は" + this.win +"勝" +this.lose+"敗" +this.draw+"分、勝率は実装中です。");
+        System.out.println(this.name + "の2022年の成績は" + this.win +"勝" +this.lose+"敗" +this.draw+"分、勝率は"+this.getRate()+"です。");
     }
 
 }
